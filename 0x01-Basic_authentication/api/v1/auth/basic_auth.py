36b8341ac2d632b6b2f7a0f9ad1beb1012be9616
[esekyi] <[sskert10@gmail.com]>
2024-11-12 16:44:28 +0000

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,14 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ Returns user email and password from decoded Base64 string
+        """
+        if decoded_base64_authorization_header is None or type(
+            decoded_base64_authorization_header
+        ) is not str or ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        return tuple(decoded_base64_authorization_header.split(":", 1))
