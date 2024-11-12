b4d6aac1528525993081afb04591fae45a1a6bda
[esekyi] <[sskert10@gmail.com]>
2024-11-12 15:24:47 +0000

    7. Basic - Base64 part- fix pycodestle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,13 @@ class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ Returns the Base64 part of the Authorization header
         """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or type(
+                authorization_header
+        ) is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
